058d90465fb3a91a780e033ff621fba65af5e622
odwyem14 <odwyem14@tcd.ie>
2025-09-20 18:55:20 +0100

    fixed links layout

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,30 @@ export default function Home() {
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="font-serif">Mairéad O'Dwyer</h1>
         <p>Integrated Computer Science student at Trinity College Dublin</p>
-        <div>
+        <div className="mt-6">
          {/* <a href="/resume.pdf" className="border round-lg">Resume</a> */}
-         <a href="https://github.com/mairead-odwyer14" target="_blank">GitHub</a>
-         <a href="https://www.linkedin.com/in/mairead-o-dwyer-a89b4a305/" target="_blank">LinkedIn</a>
+         <a 
+         href="https://github.com/mairead-odwyer14" 
+         target="_blank"
+         rel="noopener noreferrer"
+         className="block underline mb-2"
+         >
+          GitHub 
+         </a>
+         <a 
+         href="https://www.linkedin.com/in/mairead-o-dwyer-a89b4a305/" 
+         target="_blank"
+         rel="noopener noreferrer"
+         className="block underline mb-2"
+         >
+          LinkedIn
+         </a>
         </div>
+
+        <h2 className="font-sans">About Me!</h2>
+        <p className="mb-2">I am a third year Integrated Computer Science student at Trinity College Dublin.
+
+        </p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
