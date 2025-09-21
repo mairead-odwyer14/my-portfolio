fdd74a2c2cea140132f8b37af2ea7b2abc2d2445
odwyem14 <odwyem14@tcd.ie>
2025-09-21 16:43:57 +0100

    added styles such as new font, border and colour to about me section

--- src/app/page.js
+++ src/app/page.js
@@ -2,37 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="font-serif">Mairéad O'Dwyer</h1>
-        <p>Integrated Computer Science student at Trinity College Dublin</p>
-        <div className="mt-6">
+    <div>
+      <main>
+        <h1 className="font-serif text-center text-xl" >Mairéad O'Dwyer</h1>
+        <p className="font-serif text-center text-base">Integrated Computer Science student at Trinity College Dublin</p>
+        <div className="text-center gap-4 mt-2">
          {/* <a href="/resume.pdf" className="border round-lg">Resume</a> */}
-         <a 
-         href="https://github.com/mairead-odwyer14" 
-         target="_blank"
-         rel="noopener noreferrer"
-         className="block underline mb-2"
-         >
-          GitHub 
-         </a>
-         <a 
-         href="https://www.linkedin.com/in/mairead-o-dwyer-a89b4a305/" 
-         target="_blank"
-         rel="noopener noreferrer"
-         className="block underline mb-2"
-         >
-          LinkedIn
-         </a>
+         <a href="https://github.com/mairead-odwyer14" target="_blank" rel="noopener noreferrer" className="underline">GitHub </a>
+         <span>|</span>
+         <a href="https://www.linkedin.com/in/mairead-o-dwyer-a89b4a305/" target="_blank"rel="noopener noreferrer"className="underline mt-2">LinkedIn </a>
         </div>
-        <h2 className="font-sans">About Me!</h2>
-        <p className="mb-2">I am a third year Integrated Computer Science student at Trinity College Dublin (TCD). 
+        <div className="container mx-0 px-100 mt-4">
+        <h2 className="font-fascinate text-center text-purple-500">About Me!</h2>
+        <p className="rounded border-10 border-blue-200 mb-2">I am a third year Integrated Computer Science student at Trinity College Dublin (TCD). 
           I am interested in algorithms and data structures, backend, mathematics and statistics.
           I have worked as a research assistant at TCD's Complex Software Lab and I am currently a demonstrator for a statistics module.
           I have experience in Java, C, R and ARM Assembly.
           Outside of computer science, I enjoy reading, sports and baking delicious treats. 
         </p>
-        <div>
+        </div>
+        <div className="mt-4">
           <h2>Experience</h2>
           <h3>Research Assistant</h3>
           <p>
