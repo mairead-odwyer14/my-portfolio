5252a36d9230bcd212584893086687e6f06077b2
odwyem14 <odwyem14@tcd.ie>
2025-09-28 22:07:01 +0100

    made the about me section more responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import ExperienceCard from '../components/ExperienceCard';
+import HeaderCard from '@/components/HeaderCard';
 
   const experiences = [
         {
@@ -23,8 +24,8 @@ import ExperienceCard from '../components/ExperienceCard';
             My role was to annotate rugby matches using a performance analysis software, Hudl Sportscode.
             The goal of the project was to use the data to determine if the height of tackle in rugby should be lower from the shoulder to the sternum.
             The process was split into two elements, coding and labeling.
-            Coding meant that marking down possession, tackles, rucks, passes, kicks and referees' decisions.
-            Labeling was for tackles only, where aspects such as type of tackle, head contact for tackle and ball carrier, etc would be marked for each tackle.
+            Coding meant marking down possession, tackles, rucks, passes, kicks and referees' decisions.
+            Labeling was for tackles only, where aspects such as type of tackles, head contact for the tackler and ball carrier, etc would be marked for each tackle.
             I worked alongside a team of annotators, including a fellow undergrad, a PhD student and two professional rugby players.
             I throughly enjoyed my time as a research assistant. I learned a lot on how these projects are conducted.
             It was great to be able to talk to other memembers of the lab, mainly PhD students and post-doctoral researchers, as I was able to ask for advice and what their experience in computer science was like.`,    
@@ -34,54 +35,54 @@ import ExperienceCard from '../components/ExperienceCard';
 export default function Home() {
   return (
     <div>
+      <HeaderCard />
       <main>
-        <div>
-        <h1 className="font-serif text-center text-xl" >Mairéad O'Dwyer</h1>
-        <p className="font-serif text-center text-base">Integrated Computer Science student at Trinity College Dublin</p>
-        <div className="text-center gap-4 mt-2">
-         {/* <a href="/resume.pdf" className="border round-lg">Resume</a> */}
-         <a href="https://github.com/mairead-odwyer14" target="_blank" rel="noopener noreferrer" className="underline">GitHub </a>
-         <span>|</span>
-         <a href="https://www.linkedin.com/in/mairead-o-dwyer-a89b4a305/" target="_blank"rel="noopener noreferrer"className="underline mt-2">LinkedIn </a>
-        </div>
-        </div>
-        <section id="about">
-        <h2 className="font-fascinate text-center text-purple-500">About Me!</h2>
-        <div className="flex justify-start items-start ml-5">
+        <section id="about" className="scroll-mt-24 py-10 bg-cyan-100">
+        <div className="max-w-6xl mx-auto px-4">
+        <h2 className="font-fascinate text-center text-purple-500 text-3xl md:text-4xl mb-6">About Me!</h2>
+
+        <div className="flex justify-between flex-col md:flex-row items-center md:items-start gap-6">
+          <div className="flex gap-4">
           <Image
             src="/coffee_cake.jpg"
             alt="Coffee cake slice"
             width={175}
             height={175}
-            className="rounded-full border-5 border-blue-300"
+            className="rounded-full border-5 border-blue-300 object-cover"
             priority
           />
           <Image
-          src="/maths.jpg"
-          alt="Math equations on chalkboard"
-          width={175}
-          height={175}
-          className="rounded-full border-5 border-blue-300 mr-2 mt-22"
+            src="/maths.jpg"
+            alt="Math equations on chalkboard"
+            width={175}
+            height={175}
+            className="rounded-full border-5 border-blue-300 object-cover"
           />
-        <p className="text-center mx-0 rounded border-2 border-blue-200 p-4">I am a third year Integrated Computer Science student at Trinity College Dublin (TCD). 
+          </div>
+          <div className="w-full md:basis-1/2 lg:basis-3/5 xl:basis-2/3">
+        <p className="w-full rounded border-2 border-blue-200 p-6 bg-teal-100 leading-relaxed text-base md:text-lg">I am a third year Integrated Computer Science student at Trinity College Dublin (TCD). 
           I am interested in algorithms and data structures, backend, mathematics and statistics.
           I have worked as a research assistant at TCD's Complex Software Lab and I am currently a demonstrator for a statistics module.
           I have experience in Java, C, R and ARM Assembly.
           Outside of computer science, I enjoy reading, watching films and sports as well as baking delicious treats. 
         </p>
+        </div>
+        <div className="flex gap-4">
         <Image
-        src="/programming.jpg"
-        alt="Programming languages"
-        width={175}
-        height={175}
-        className="rounded-full border-5 border-blue-300 ml-2"
+          src="/programming.jpg"
+          alt="Programming languages"
+          width={175}
+          height={175}
+          className="rounded-full border-5 border-blue-300 object-cover"
         />
         <Image
-        src="/Trinity-Main-Logo.jpg"
-        alt="Trinity College Dublin Logo"
-        width={200}
-        height={200}
-        className="rounded-full border-5 border-blue-300 mt-36"/>
+          src="/Trinity-Main-Logo.jpg"
+          alt="Trinity College Dublin Logo"
+          width={200}
+          height={200}
+          className="rounded-full border-5 border-blue-300 object-cover"/>
+        </div>
+        </div>
         </div>
         </section>
         <section id="experience" className="py-6 bg-purple-100">
