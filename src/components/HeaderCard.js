18e3af70b03b5b0b01a2318a64e27da5c11992ea
odwyem14 <odwyem14@tcd.ie>
2025-09-28 14:31:27 +0100

    created component for header, added the main details

---
+++ src/components/HeaderCard.js
@@ -0,0 +1,29 @@
+export default function HeaderCard(){
+    return(
+        <header className="bg-blue-100 w-full">
+            <div className="text-center mb-5">
+                <h1 className="font-serif text-center text-xl" >Mairéad O'Dwyer</h1>
+                <p className="font-serif text-center text-base">Integrated Computer Science student at Trinity College Dublin</p>
+                <div className="flex justify-center gap-4 mt-2">
+                    <a 
+                        href="https://github.com/mairead-odwyer14" 
+                        target="_blank" 
+                        rel="noopener noreferrer" 
+                        className="underline"
+                    >
+                        GitHub 
+                    </a>
+                    <span>|</span>
+                    <a 
+                        href="https://www.linkedin.com/in/mairead-o-dwyer-a89b4a305/" 
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="underline mt-2"
+                    >
+                        LinkedIn 
+                    </a>
+                </div>
+            </div>
+        </header>
+    )
+}
