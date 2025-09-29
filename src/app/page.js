386b704ee40246ba4ed0b33a6673277ff805684e
odwyem14 <odwyem14@tcd.ie>
2025-09-29 22:05:41 +0100

    changed colour scheme on main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import ExperienceCard from '../components/ExperienceCard';
 import HeaderCard from '@/components/HeaderCard';
+import ProjectCard from '../components/ProjectCard';
 
   const experiences = [
         {
@@ -32,61 +33,56 @@ import HeaderCard from '@/components/HeaderCard';
     },
   ];
 
+  const projects = [
+    {
+      title: "Portfolio",
+      tools: "Next.js, Javascript, Tailwind CSS, Git",
+      duration: "September 2025 - Present",
+      intro: `I decided to create a personal portfolio page to gain experience in frontend programming.
+              I researched different frameworks to use and I decided to use Next.js.
+              I also used Tailwind CSS to style the different elements of the portfolio as it is more clean than using CSS files.`,
+      contributions: [],
+    },
+    {
+      title: "TCD Student Job Portal",
+      tools: "Java, SpringBoot, REST, Postman, Git",
+      duration: "January 2025 - May 2025",
+      intro: `I was a member of the backend team for a module project. 
+              It was a job portal for Trinity students which would allow them to apply for jobs posted by companies. 
+              It was done in partnership with Liferay.
+              My main contributions were:
+              `,
+      contributions: [
+        "Created backend structure for messaging and notifications, such as domains, DTOs, repositiories, controllers and services.",
+        "Designed and implemented REST endpoints to fetch students, job listings and applications.",
+        "Verified REST endpoints using Postman to ensure correct functionality.",
+        "Contributed to team demos."
+      ],
+    },
+  ];
+
 export default function Home() {
   return (
     <div>
       <HeaderCard />
       <main>
-        <section id="about" className="scroll-mt-24 py-10 bg-cyan-100">
+        <section id="about" className="scroll-mt-24 py-10">
         <div className="px-4">
-        <h2 className="font-fascinate text-center text-purple-500 text-3xl md:text-4xl mb-6">About Me!</h2>
-
+        <h2 className="font-serif text-center text-black-100 text-3xl md:text-4xl mb-6">About Me!</h2>
         <div className="flex flex-col md:flex-row items-start justify-between gap-8 md:gap-12 xl:gap-16">
-          <div className="flex gap-4 self-start shrink-0">
-          <Image
-            src="/coffee_cake.jpg"
-            alt="Coffee cake slice"
-            width={175}
-            height={175}
-            className="rounded-full border-5 border-blue-300 object-cover"
-            priority
-          />
-          <Image
-            src="/maths.jpg"
-            alt="Math equations on chalkboard"
-            width={175}
-            height={175}
-            className="rounded-full border-5 border-blue-300 object-cover"
-          />
           </div>
           <div className="w-full md:basis-1/2 lg:basis-3/5 xl:basis-2/3 md:mx-2">
-        <p className="w-full rounded border-2 border-blue-200 p-6 bg-teal-100 leading-relaxed text-base md:text-lg">I am a third year Integrated Computer Science student at Trinity College Dublin (TCD). 
+        <p className="w-full rounded border-2 border-blue-200 bg-blue-100 p-6 leading-relaxed text-base md:text-lg">I am a third year Integrated Computer Science student at Trinity College Dublin (TCD). 
           I am interested in algorithms and data structures, backend, mathematics and statistics.
           I have worked as a research assistant at TCD's Complex Software Lab and I am currently a demonstrator for a statistics module.
           I have experience in Java, C, R and ARM Assembly.
           Outside of computer science, I enjoy reading, watching films and sports as well as baking delicious treats. 
         </p>
         </div>
-        <div className="flex gap-4 ml-auto self-start shrink-0">
-        <Image
-          src="/programming.jpg"
-          alt="Programming languages"
-          width={175}
-          height={175}
-          className="rounded-full border-5 border-blue-300 object-cover"
-        />
-        <Image
-          src="/Trinity-Main-Logo.jpg"
-          alt="Trinity College Dublin Logo"
-          width={175}
-          height={175}
-          className="rounded-full border-5 border-blue-300 object-cover"/>
-        </div>
-        </div>
         </div>
         </section>
-        <section id="experience" className="scroll-mt-24 py-6 bg-purple-100">
-          <h1 className="font-audiowide p-4 text-2xl">Experience</h1>
+        <section id="experience" className="scroll-mt-24 py-6">
+          <h1 className="font-serif p-4 text-2xl">Experience</h1>
           <div className="flex flex-wrap justify-center items-center p-4 gap-6">
             {experiences.map((experience, index) => (
               <ExperienceCard
@@ -100,10 +96,19 @@ export default function Home() {
             ))}
           </div>
         </section>
-        <section id="projects" className="scroll=mt-24 py-6 bg-pink-100">
+        <section id="projects" className="scroll=mt-24 py-6">
           <h1 className="font-bitcount p-4 text-2xl">Projects</h1>
           <div className="flex flex-wrap justify-center items-center p-4 gap-6">
-            
+            {projects.map((project, index) => (
+              <ProjectCard
+              key={index}
+              title={project.title}
+              tools={project.tools}
+              duration={project.duration}
+              intro={project.intro}
+              contributions={project.contributions}
+              />
+            ))}
           </div>
 
         </section>
